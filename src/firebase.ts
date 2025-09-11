ecb76fb86b39b6382146189864328cb00253be2d
hellwind2019 <daniltorianik2016@gmail.com>
2025-09-11 18:09:36 +0300

    fixes

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,14 +1,14 @@
-// import { Bucket } from "@google-cloud/storage"; // import type
-// import admin from "firebase-admin";
-// import { getStorage } from "firebase-admin/storage";
-// import path from "path";
+import { Bucket } from "@google-cloud/storage"; // import type
+import admin from "firebase-admin";
+import { getStorage } from "firebase-admin/storage";
+import path from "path";
 
-// const serviceAccount = path.resolve(
-//   "superherodatabase-a38b1-firebase-adminsdk-fbsvc-53fcad6686.json"
-// );
-// admin.initializeApp({
-//   credential: admin.credential.cert(serviceAccount),
-//   storageBucket: "superherodatabase-a38b1.firebasestorage.app", // ⚠️ must be .appspot.com
-// });
-// export const bucket: Bucket = getStorage().bucket() as any;
-// // Initialize Firebase
+const serviceAccount = path.resolve(
+  "superherodatabase-a38b1-firebase-adminsdk-fbsvc-53fcad6686.json"
+);
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  storageBucket: "superherodatabase-a38b1.firebasestorage.app", // ⚠️ must be .appspot.com
+});
+export const bucket: Bucket = getStorage().bucket() as any;
+// Initialize Firebase
--- src/routes/superheroes.ts
+++ src/routes/superheroes.ts
@@ -2,7 +2,7 @@ import { Router } from "express";
 import pool from "../db/db.js";
 
 import multer from "multer";
-// import { bucket } from "../firebase.js";
+import { bucket } from "../firebase.js";
 const router = Router();
 import type { Request } from "express";
 
@@ -89,29 +89,29 @@ router.put("/:id", async (req, res) => {
 
 const upload = multer({ storage: multer.memoryStorage() });
 
-// router.post("/:id/images", upload.single("image"), async (req, res) => {
-//   try {
-//     const id = req.params.id;
-//     if (!req.file) return res.status(400).send("No file uploaded");
+router.post("/:id/images", upload.single("image"), async (req, res) => {
+  try {
+    const id = req.params.id;
+    if (!req.file) return res.status(400).send("No file uploaded");
 
-//     // File name inside Firebase
-//     const fileName = `superheroes/${id}/${Date.now()}_${req.file.originalname}`;
-//     const file = bucket.file(fileName);
+    // File name inside Firebase
+    const fileName = `superheroes/${id}/${Date.now()}_${req.file.originalname}`;
+    const file = bucket.file(fileName);
 
-//     // Upload buffer to Firebase
-//     await file.save(req.file.buffer, {
-//       metadata: { contentType: req.file.mimetype },
-//     });
+    // Upload buffer to Firebase
+    await file.save(req.file.buffer, {
+      metadata: { contentType: req.file.mimetype },
+    });
 
-//     // Make public (optional)
-//     await file.makePublic();
-//     const publicUrl = `https://storage.googleapis.com/${bucket.name}/${fileName}`;
+    // Make public (optional)
+    await file.makePublic();
+    const publicUrl = `https://storage.googleapis.com/${bucket.name}/${fileName}`;
 
-//     res.json({ url: publicUrl });
-//   } catch (err) {
-//     console.error(err);
-//     res.status(500).send("Error uploading image");
-//   }
-// });
+    res.json({ url: publicUrl });
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Error uploading image");
+  }
+});
 
 export default router;
