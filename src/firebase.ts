f077eb87d39d4efbfdfbff9378c6f7fc2721638b
hellwind2019 <daniltorianik2016@gmail.com>
2025-09-12 11:17:36 +0300

    fix path

--- src/firebase.ts
+++ src/firebase.ts
@@ -4,7 +4,7 @@ import { getStorage } from "firebase-admin/storage";
 import path from "path";
 
 const serviceAccount = path.resolve(
-  "secrets/superherodatabase-a38b1-firebase-adminsdk-fbsvc-53fcad6686.json"
+  "superherodatabase-a38b1-firebase-adminsdk-fbsvc-53fcad6686.json"
 );
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
